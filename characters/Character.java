6309540a0cc85cb79de7c1283128749b479bde34
Carlos <carlos.vlp@discente.ufma.br>
2024-05-21 16:27:41 -0300

    Update Character.java

--- characters/Character.java
+++ characters/Character.java
@@ -21,10 +21,21 @@ public abstract class Character {
     aqui é usado abstract porque o comportamento desses metodos vão ser distintos em cada tipo de personagem
     então o abstract permite que a gente possa fazer modificações nas funções em cada caso sem afetar o resto
     */
-    
     public abstract int attack();
     public abstract boolean recieveDamage(int damage);
-    public abstract boolean showDetails();
+    public abstract void levelUp();
+
+    public void showDetails(){}
+    public void showDetails(Character p1, Character p2){
+        p1.showDetails();
+        p2.showDetails();
+        /*Coloquei essa função pq acho que talvez seja necessario chamar ela pra mostrar os detalhes de cada
+         * personagem envolvido no combate (aqui é polimorfodase, ja que são dois metodos com o mesmo nome)
+         * 
+         * --> MAS NÃO SEI SE VAI FUNCIONAR DESSE JEITO Q EU COLOQUEI AQUI!!! <--
+         */
+
+    }
     
     /*Aqui é o metodo que a gente vai utilizar pra calcular a probabilidade, então por ex:
       se o arqueiro tiver 40% de chance de acertar uma flecha, essa probabilidade vai ser
